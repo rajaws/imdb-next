35166a713257886b2193dcb4b510aea2b237185d
waqarshaukat <waqarshaukat747@gmail.com>
2024-04-19 12:49:27 +0500

    add dark mode

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function DarkModeSwitch() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const[mounted,setMounted]=useState(false);
+  const currenttheme = theme === "system" ? systemTheme : theme;
+  useEffect(()=>setMounted(true),[]);
+
+
+  return (
+    <div>
+      {mounted && (currenttheme === "dark" ? (
+        <MdLightMode onClick={()=>setTheme("light")} className=" text-xl cursor-pointer hover:bg-amber-500" />
+      ) : (
+        <MdDarkMode onClick={()=>setTheme("dark")} className=" text-xl cursor-pointer hover:bg-amber-500" />
+      ))}
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import MenuItem from './MenuItem'
+import React from "react";
+import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillCircleFill } from "react-icons/bs";
-import Link from 'next/link';
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className=' flex justify-between items-center p-3 max-w-6xl mx-auto'>
-        <div className=' flex gap-4'>
-            <MenuItem title="home" address="/" Icon={AiFillHome}/>
-            <MenuItem title="about" address="/about" Icon={BsFillCircleFill}/>
-        </div>
-        <Link href={"/"} className='flex gap-1 items-center'>
-            <span className=' text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg '>IMDB</span>
-            <span className=' text-xl hidden sm:inline'>Clone</span>
+    <div className=" flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <div className=" flex gap-4">
+        <MenuItem title="home" address="/" Icon={AiFillHome} />
+        <MenuItem title="about" address="/about" Icon={BsFillCircleFill} />
+      </div>
+      <div className=" flex items-center gap-4">
+        <DarkModeSwitch />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className=" text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg ">
+            IMDB
+          </span>
+          <span className=" text-xl hidden sm:inline">Clone</span>
         </Link>
+      </div>
     </div>
-  )
+  );
 }
