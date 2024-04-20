85fd69561cfa6c0d2a40293a5daa753cf68ba521
waqarshaukat <waqarshaukat747@gmail.com>
2024-04-20 14:03:21 +0500

    add search box

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import NavbarItem from "./NavbarItem";
+import SearchBox from "./SearchBox";
 
 export default function Navbar() {
   return (
     <div className="flex dark:bg-gray-600 bg-amber-200 p-4 lg:text-lg justify-center gap-6 ">
         <NavbarItem title="Trending" param="fetchTrending"/>
         <NavbarItem title="Top Rated" param="fetchTopRated"/>
+        
     </div>
   )
 }
